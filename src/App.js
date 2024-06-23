04b8cadf1bee7413bbf711f60d27941baaacc20c
HBADE2002 <hrishikeshbade007@gmail.com>
2024-06-23 18:14:55 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import Chatbot from './components/Chatbot';
+import NavigationBar from './components/NavigationBar';
 
 const App = () => {
   return (
     <div>
+      <NavigationBar/>
       <Chatbot />
+      
+
     </div>
   );
 };
