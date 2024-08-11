eb8a6c76d3b49b5c21c7fd4b47b45356cb7378ac
HBADE2002 <hrishikeshbade007@gmail.com>
2024-08-11 21:29:18 +0530

    new changes 2

--- src/App.js
+++ src/App.js
@@ -2,8 +2,6 @@ import React from 'react';
 import Chatbot from './components/Chatbot';
 import NavigationBar from './components/NavigationBar';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import About from './components/About'; // Import the About page
 // Import other pages as needed
 
 const App = () => {
