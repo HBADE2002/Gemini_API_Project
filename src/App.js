4920286f826aa2957bcb7d6d8b8266083000015d
HBADE2002 <hrishikeshbade007@gmail.com>
2024-08-11 21:39:35 +0530

    new changes 3

--- src/App.js
+++ src/App.js
@@ -1,18 +1,15 @@
-import React from 'react';
-import Chatbot from './components/Chatbot';
-import NavigationBar from './components/NavigationBar';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import React from "react";
+import Chatbot from "./components/Chatbot";
+import NavigationBar from "./components/NavigationBar";
+import "bootstrap/dist/css/bootstrap.min.css";
 // Import other pages as needed
 
 const App = () => {
   return (
-    <Router>
+    <div>
       <NavigationBar />
-      <Routes>
-        <Route path="/about" element={<About />} />
-      </Routes>
       <Chatbot />
-    </Router>
+    </div>
   );
 };
 
